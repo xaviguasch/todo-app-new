fd5e27d910269254e2369f2476d64a2579a0be5f
xaviguasch <xaviguasch@gmail.com>
2018-10-22 12:14:23 +0200

    refactoring with some light destructuring

--- src/views.js
+++ src/views.js
@@ -4,11 +4,11 @@ import { getFilters } from './filters'
 
 const renderTodos = () => {
     const todoEl = document.querySelector('#todos')
-    const filters = getFilters()
+    const { searchText, hideCompleted } = getFilters()
 
     const filteredTodos = getTodos().filter((todo) => {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+        const searchTextMatch = todo.text.toLowerCase().includes(searchText.toLowerCase())
+        const hideCompletedMatch = !hideCompleted || !todo.completed
 
         return searchTextMatch && hideCompletedMatch
     })
